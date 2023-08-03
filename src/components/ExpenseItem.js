948ee2ebf4533cdd93685beac6609ab8d37a92fe
3 <34marqam@gmail.com>
2023-08-02 21:22:00 -0400

    completed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import {FaPlusCircle, FaMinusCircle} from 'react-icons/fa';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -25,12 +26,34 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td><FaPlusCircle
+                size='1.5em'
+                style={{color:'green'}}
+                onClick={event=> increaseAllocation(props.name)} 
+            /> </td>
+            <td><FaMinusCircle
+                size='1.5em'
+                style={{color:'red'}}
+                onClick={event=> {decreaseAllocation(props.name)}}
+            /></td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
